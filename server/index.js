910d0f17ab532c69bedf0f6cf364924e261b814a
tanuvit16 <tanuvit16@gmail.com>
2026-04-03 11:18:31 +0530

    deploy backend

--- server/index.js
+++ server/index.js
@@ -15,7 +15,11 @@ const app  = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://eatoza-v2.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
